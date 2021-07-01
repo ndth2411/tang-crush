48daf38c807018363db1e26407192aa7ab4b2a8f
ndth2411 <86772761+ndth2411@users.noreply.github.com>
2021-07-01 16:30:00 +0700

    Update config.js

--- config.js
+++ config.js
@@ -7,13 +7,13 @@ const CONFIG = {
     btnIntro: 'hihi',
     title: 'Đêm về nghe lòng thương em mất rồi, lại ngại vì mình con trai :((',
     desc: 'Phải làm sao? ',
-    btnYes: 'Thích thì yêu hong yêu thì yêu hihi ',
+    btnYes: 'Thích thì yêu hong yêu thì yêu hihi',
     btnNo: 'No, we just friend :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
     reply: '<3',
     mess: 'Hihi iu bé nhìuuu <3',
-    messDesc: 'Hong được viết gì xấu đâu nhó .',
+    messDesc: 'Hong được viết gì xấu đâu nhó',
     btnAccept: 'oklaaaaa :',
     messLink: 'https://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
